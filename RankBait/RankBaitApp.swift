e3c0a93dc07023127edec38a6af9e5ae566f7053
Andy Li <andy.dang.li@gmail.com>
2025-10-27 19:12:55 -0400

    Initial Commit

---
+++ RankBait/RankBaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  RankBaitApp.swift
+//  RankBait
+//
+//  Created by AndyDLi on 10/27/25.
+//
+
+import SwiftUI
+
+@main
+struct RankBaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
